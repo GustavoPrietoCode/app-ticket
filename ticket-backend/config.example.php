d17805e4fc3849be6843205e32b075c98a255520
gustavo <gustavo@estudionexos.com>
2026-08-11 09:43:35 +0200

    change gitigonre and token

---
+++ ticket-backend/config.example.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'db' => [
+        'host' => 'host',
+        'port' => 0000,
+        'name' => 'name_db',
+        'user' => 'root',
+        'pass' => '',
+        'charset' => 'utf8mb4',
+    ],
+    'gitea' => [
+        'base_url' => 'https://example.com',
+        'token'    => '',
+        'owner'    => 'name',
+        'repo'     => 'repo-name',
+    ],
+];
